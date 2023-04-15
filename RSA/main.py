47546bbbfd39e738bc35d390d73acddd18a63f38
HLRichardson <101138530+HLRichardson-Git@users.noreply.github.com>
2023-04-14 21:07:18 -0500

    Create main.py

---
+++ RSA/main.py
@@ -0,0 +1,25 @@
+import math
+
+from utils import Key
+
+
+def encrypt(x, e, n):
+    return pow(x, e, n)
+
+def decrypt(y, d, n):
+    return pow(y, d, n)
+
+def main():
+    k = Key()
+    
+    plaintext_message = int(input("Enter plain text here:"))
+    ciphertext_message = encrypt(plaintext_message, k.e, k.n)
+    print("Encrypted ciphertext: " + str(ciphertext_message))
+
+    plaintext_message = decrypt(ciphertext_message, k.d, k.n)
+    print("Decrypted plaintext: " + str(plaintext_message))
+
+
+
+if __name__ == '__main__':
+    main()
